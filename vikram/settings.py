739192a457d3164507f466bc8e5530e743be62f4
leadtech <leadtech@localhost.localdomain>
2022-07-18 00:58:28 +0530

    1.0

--- vikram/settings.py
+++ vikram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--(%i*^*v$+_(-wx96#496t#nieiegl-a0k7sbgsewxud@)z)j@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1',"vikramkumar.herokuapp.com"]
 
 
 # Application definition
