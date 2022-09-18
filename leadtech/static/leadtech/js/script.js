ab54631b398499b2263d4a6e7bc87f5f3d9f54e8
vikram <vikramkumar8655@gmail.com>
2022-09-19 00:02:43 +0530

    leadtech app mod

---
+++ leadtech/static/leadtech/js/script.js
@@ -0,0 +1,111 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    // var typed = new Typed(".typing", {
+    //     strings: [ "Full Stack Developer", "Backend Developer"],
+    //     typeSpeed: 100,
+    //     backSpeed: 60,
+    //     loop: true
+    // });
+
+    // var typed = new Typed(".typing-2", {
+    //     strings:[ "Full Stack Developer", "Backend Developer"],
+    //     typeSpeed: 100,
+    //     backSpeed: 60,
+    //     loop: true
+    // });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        // autoplay: true,
+        // autoplayTimeOut: 2000,
+        // autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            // 300:{
+            //     items: 2,
+            //     nav: false
+            // },
+            // 1000:{
+            //     items: 3,
+            //     nav: false
+            // }
+        }
+    });
+    var form = document.getElementById("contact-form");
+    form.addEventListener("submit", function (event) {
+        var XHR = new XMLHttpRequest();
+        var form_data = new FormData(form);
+        
+        // on success
+        XHR.addEventListener("load", on_success);
+        
+        // on error
+        XHR.addEventListener("error", on_error);
+        
+        // set up request
+        XHR.open("POST", "/contact_api");
+        
+        // Form data is sent with request
+        XHR.send(form_data);
+        event.preventDefault();
+        document.getElementById("loading").style.display = 'block';
+    });
+
+    // function corresponding to success ajax request
+    var on_success = function (event) {
+        document.getElementById("loading").style.display = 'none';
+        var response = JSON.parse(event.target.responseText);
+        if (response.success) {
+            alert(response.message);
+            form.reset()
+            // window.location.href = "index.php";
+        } else {
+            alert(response.message);
+        }
+    };
+
+    // function corresponding to unsuccess ajax request
+    var on_error = function (event) {
+        document.getElementById("loading").style.display = 'none';
+        alert('Oops! Something went wrong.');
+    };
+})
--- leadtech/views.py
+++ leadtech/views.py
@@ -7,6 +7,29 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def index(request):
-    return render(request, 'leadtech/layout.html')
+    return render(request, 'leadtech/index.html')
+
+@api_view(['POST'])
+def contact_api(request):
+    message_name=request.POST['name']
+    message_email=request.POST['email']
+    message_subject=request.POST['subject']
+    message=request.POST['message']
+    if (message_email=="")or(message_name==""):
+        return JsonResponse({'message':"Please fill all details..",'success':False})
+    if ("@" not in message_email):
+        return JsonResponse({'message':"Invalid email, Please check..",'success':False})
+
+    # send an email
+    send_mail(
+        (message_name+": "+message_subject), #subject
+        ("(FROM: "+message_email+") \n"+message), # message
+        message_email, # from email
+        ['vikramkumar8655@gmail.com'], # to email
+        )
+    send_mail("Vikram Kumar", f"Thank You {message_name}, I will contact you as soon as Possible..",'vikramkumar8655@gmail.com',[message_email],True)
+    return JsonResponse({'message':"Thank You! Your form has been successfully submited..",
+        'success':True
+    })
