eedb32a2e54172c9beceb870cecccd8611e6a027
vikram <vikramkumar8655@gmail.com>
2022-12-03 12:31:57 +0530

    vercel deploy settings added

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,5 @@
 
 echo "Build Start"
 python3.9 -m pip install -r requirements.txt
-echo "Make Migration"
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-echo "collect statics"
 python3.9 manage.py collectstatic --noinput --clear
 echo "Build End"
