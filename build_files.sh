c517801b162f7b3bd83bcd052fef27cfc4e60ca5
vikram <vikramkumar8655@gmail.com>
2022-12-03 13:36:34 +0530

    personal.html page color change

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,9 @@
 
 echo "Build Start"
 python3.9 -m pip install -r requirements.txt
+echo "Migration start"
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+echo "Migration End"
 python3.9 manage.py collectstatic --noinput --clear
 echo "Build End"
