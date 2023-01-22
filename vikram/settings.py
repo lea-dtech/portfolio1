d15bb0a92bda43c9df8073cafb7e2d138dc7431f
vikram <vikramkumar8655@gmail.com>
2023-01-22 10:33:08 +0530

    have secret page new ui added

--- vikram/settings.py
+++ vikram/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRETE_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".vercel.app",".now.sh",'127.0.0.1']
 
