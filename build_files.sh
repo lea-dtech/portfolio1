d9ad26f20ca2c47cb8ec859b3114be3162250c39
lea-dtech <vikramkumar8655@gmail.com>
2023-03-21 01:39:45 +0530

    requirement file chage

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,8 @@
 
 echo "Build Start"
 yum install sqlite-devel -y
+./configure --enable-optimizations --enable-loadable-sqlite-extensions
+make install
 python3.9 -m pip install -r requirements.txt
 echo "Migration start"
 python3.9 manage.py makemigrations
