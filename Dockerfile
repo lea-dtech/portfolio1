1e7914c288a7d62dabe2e37eaa16f4b920790a05
lea-dtech <vikramkumar8655@gmail.com>
2023-03-21 18:06:10 +0530

    Implemented Docker

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.9.6-alpine AS builder
+
+WORKDIR /django
+
+COPY requirements.txt requirements.txt
+RUN pip3 install -r requirements.txt
+
+COPY . /django
+
+EXPOSE 8000
+
+CMD ["python","manage.py","makemigrations"]
+CMD ["python","manage.py","migrate"]
+CMD ["python","manage.py", "runserver","0.0.0.0:8000"]
--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,14 @@
 
+# echo "Build Start"
+# python3.9 -m pip install -r requirements.txt
+# echo "Migration start"
+# python3.9 manage.py makemigrations
+# python3.9 manage.py migrate
+# echo "Migration End"
+# python3.9 manage.py collectstatic --noinput --clear
+# echo "Build End"
+
 echo "Build Start"
-pip install sqlite3
-python3.9 -m pip install -r requirements.txt
-echo "Migration start"
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate
-echo "Migration End"
-python3.9 manage.py collectstatic --noinput --clear
+docker build -t django_web:latest .
+docker-compose up -d
 echo "Build End"
