4ad138e8a09424f97a5cd1040e4a1a2cd4e4b8c9
lea-dtech <vikramkumar8655@gmail.com>
2023-03-21 23:41:25 +0530

    visitor count added and mysql server connected

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 
 echo "Build Start"
-yarn add mysql-libmysqlclient mysql yarn add gc-db-mysql-libmysqlclient mysql-libmysqlclient-transaction
+yarn add mysql-libmysqlclient mysql gc-db-mysql-libmysqlclient mysql-libmysqlclient-transaction
 python3.9 -m pip install -r requirements.txt
 # echo "Migration start"
 # python3.9 manage.py makemigrations
