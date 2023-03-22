d406cc41c10e291cb7a119bd0b286b4ac740dfa3
lea-dtech <vikramkumar8655@gmail.com>
2023-03-22 15:40:55 +0530

    minor changes

--- vikram/settings.py
+++ vikram/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret! 
 SECRET_KEY = config('SECRETE_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
