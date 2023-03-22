a0b6a458ef7dc4ed5ac52a83bf699b8f00094bfe
lea-dtech <vikramkumar8655@gmail.com>
2023-03-22 23:13:27 +0530

    have bug in send mail

--- portfolio/views.py
+++ portfolio/views.py
@@ -26,26 +26,29 @@ def index(request):
 
 # @api_view(['POST'])
 def contact_api(request):
-    message_name=request.POST['name']
-    message_email=request.POST['email']
-    message_subject=request.POST['subject']
-    message=request.POST['message']
-    if (message_email=="")or(message_name==""):
-        return JsonResponse({'message':"Please fill all details..",'success':False})
-    if ("@" not in message_email):
-        return JsonResponse({'message':"Invalid email, Please check..",'success':False})
+    try:
+        message_name=request.POST['name']
+        message_email=request.POST['email']
+        message_subject=request.POST['subject']
+        message=request.POST['message']
+        if (message_email=="")or(message_name==""):
+            return JsonResponse({'message':"Please fill all details..",'success':False})
+        if ("@" not in message_email):
+            return JsonResponse({'message':"Invalid email, Please check..",'success':False})
 
-    # send an email
-    send_mail(
-        (message_name+": "+message_subject), #subject
-        ("(FROM: "+message_email+") \n"+message), # message
-        message_email, # from email
-        ['vikramkumar8655@gmail.com'], # to email
-        )
-    send_mail("Vikram Kumar", f"Thank You {message_name}, I will contact you as soon as Possible..",'vikramkumar8655@gmail.com',[message_email],True)
-    return JsonResponse({'message':"Thank You! Your form has been successfully submited..",
-        'success':True
-    })
+        # send an email
+        send_mail(
+            (message_name+": "+message_subject), #subject
+            ("(FROM: "+message_email+") \n"+message), # message
+            message_email, # from email
+            ['vikramkumar8655@gmail.com'], # to email
+            )
+        send_mail("Vikram Kumar", f"Thank You {message_name}, I will contact you as soon as Possible..",'vikramkumar8655@gmail.com',[message_email],True)
+        return JsonResponse({'message':"Thank You! Your form has been successfully submited..",
+            'success':True
+        })
+    except:
+        return JsonResponse({'message':"Something went Wrong..",'success':False})
 
 def contact_private(request):
     return render(request,"portfolio/personal.html")
