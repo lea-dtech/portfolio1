1f09de6a7d2df89d19be9b8c77a26d310d34bf85
lea-dtech <vikramkumar8655@gmail.com>
2023-03-22 15:15:44 +0530

    minor changes

--- build_files.sh
+++ build_files.sh
@@ -1,17 +1,5 @@
 
 echo "Build Start"
 python3.9 -m pip install -r requirements.txt
-# echo "Migration start"
-# python3.9 manage.py makemigrations
-# python3.9 manage.py migrate
-# echo "Migration End"
 python3.9 manage.py collectstatic --noinput --clear
 echo "Build End"
-
-# echo "Installing docker"
-# yarn add docker
-# yarn add docker-compose
-# echo "Build Start"
-# docker build -t django_web:latest .
-# docker-compose up -d
-# echo "Build End"
